a0896cd7521b772b0c23a4ad43bde81dd8fceb43
BraulioEstrada-Ksquare <Braulio.Estrada@itksquare.edu.mx>
2023-03-26 01:03:39 -0600

    primera version del proyecto, agregando estructura y estilos css

---
+++ script.js
@@ -0,0 +1,42 @@
+/*
+Pokedex App
+For this exercise, we will create an app akin to a Pokedex from the game and tv series Pokémon with the end goal of you dominating how to consume the resources from any API. 
+
+The examples that you'll find here are only for you to get inspiration as I will give points to styles, and this includes your design (feel free to look for more inspiration on the internet). 
+
+This project will be done in pairs as part of your tasks you need to have a meeting before starting to code where you'll define who will attack which User Stories, remember to use Git and GitHub for your collaborative needs, and have strong communication between you. 
+
+Let's look at the requirements for this project. 
+
+Requirements 
+
+You must use only HTML, CSS & JS without any frameworks or libraries 
+
+You must code your logic using functions in JavaScript  
+
+You must use the box model to recreate as close as possible the look and feel of the cards 
+
+You must use the Fetch API to interact with APIs 
+
+You must use the following API to get the Pokémon’s info: https://pokeapi.co but feel free to research other APIs if you wish to get extra information about them 
+
+Stories 
+
+The Pokedex must have a container with a grid of 4x3 cards 
+
+The Pokedex must have a card where the information for each Pokémon must live inside 
+
+The Pokémon card must use Flexbox 
+
+The Pokémon card must contain the following information: "name", "sprite", "types", "attack value", "hp", "defense value", "speed value" 
+
+You have to choose 12 Pokémon (this can be chosen at random if you aren't familiar with the franchise) and show them at load time
+
+As a user, I must be redirected to its Poke Wiki entry when I click the name inside the card 
+
+The Pokedex must include a search box where I can type the name of a Pokémon and its information must be the only Pokemon shown inside the grid 
+
+The Pokedex must include a clickable element that will reset the search value and return to the 12 Pokémon you initially show 
+
+The app should work and look fine in a 320px width viewport 
+*/
